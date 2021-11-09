ff709a3230695fccf6030e299bd7c0faf5c00cc0
olkinin <olkinin@mail.ru>
2021-11-09 07:54:30 +0300

    comments

---
+++ src/main/java/ru/geekbrains/lesson5/homework5/Person.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.lesson5.homework5;
+
+public class Person {
+    public String name;
+    public String position;
+    public String email;
+    public String tel;
+    public int wage;
+    public int age;
+
+    public Person(String name, String position, String email, String tel, int wage, int age) {
+    }
+
+}
