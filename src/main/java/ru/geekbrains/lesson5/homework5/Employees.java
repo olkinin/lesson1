75e8d79bcd9c6075e562ade4cc90694b1e505134
olkinin <olkinin@mail.ru>
2021-11-09 21:20:47 +0300

    comments

--- src/main/java/ru/geekbrains/lesson5/homework5/Employees.java
+++ src/main/java/ru/geekbrains/lesson5/homework5/Employees.java
@@ -1,19 +1,26 @@
 package ru.geekbrains.lesson5.homework5;
 
+import com.sun.corba.se.impl.resolver.SplitLocalResolverImpl;
+
+import java.util.Arrays;
+
 public class Employees {
+
+    private int age;
     public String name;
     public String position;
     public String email;
     public String tel;
     public int wage;
-    public int age;
+
+
     public Person[] arrPerson;
 
 
-    public void main(String[] args) {
-        new Employees("Ольга", "economist",
-                "olkinin", "3245724", 1000, 33);
-        System.out.println(Person);
+    public static void main(String[] args) {
+        new Employees("Ivan Ivanov", "economist",
+                "ivanov@yandex.ru", "2552525", 5000, 33);
+       works();
 
     }
 
@@ -34,10 +41,11 @@ public class Employees {
     public void print() {
         System.out.println(name + ", должность: " + position + ", электронный адрес: " + email +
                 ", телефон: " + tel + ", заработная плата: " + wage + ",  возраст: " + age);
+        System.out.println();
     }
 
 
-    public void works() {
+   public static void works() {
         Person[] arrPerson = new Person[5];
         arrPerson[0] = new Person("Ivanov Ivan", "Engineer",
                 "ivanov@mail.ru", "3332525", 10000, 40);
@@ -49,18 +57,17 @@ public class Employees {
                 "dariavas@mail.ru", "1112221", 9000, 42);
         arrPerson[4] = new Person("Kovaleva Maria", "Economist",
                 "maria@mail.ru", "3252525", 11000, 45);
-        printText();
 
-    }
-
-    public void printText() {
         for (int i = 0; i < arrPerson.length; i++) {
-            if (this.age > 40) {
+            if (arrPerson[i].age > 40) {
                 System.out.println(arrPerson[i]);
+            }}}
             }
-        }
-    }
-}
+
+
+
+
+
 
 
 
--- src/main/java/ru/geekbrains/lesson5/homework5/Person.java
+++ src/main/java/ru/geekbrains/lesson5/homework5/Person.java
@@ -1,14 +1,28 @@
 package ru.geekbrains.lesson5.homework5;
 
 public class Person {
+
     public String name;
     public String position;
     public String email;
     public String tel;
     public int wage;
     public int age;
+    public int length;
+
 
     public Person(String name, String position, String email, String tel, int wage, int age) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.tel = tel;
+        this.wage = wage;
+        this.age = age;
     }
+    public String toString(){
+        return String.format("Name %s: Position %s: Email %s: Tel %s: Wage %d: Age %d\n",
+                name,position,email,tel,wage,age);
+    }
+
 
 }
